fe81d0f4504e32d68e2f89a0dfca0bad3f2cd89c
Jonas Dalesjö <jonas.dalesjo@gmail.com>
2025-11-27 09:17:48 +0100

    Re-enable Next.js locale detection

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,6 @@ const nextConfig: NextConfig = {
   i18n: {
     locales: i18n.locales,
     defaultLocale: i18n.defaultLocale,
-    localeDetection: false,
   },
   experimental: {
     // @ts-ignore - cacheComponents is available in Next.js 15+ (canary/rc) or 16
