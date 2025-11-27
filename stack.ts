c200cd137d211d02fed9beb75969a6ce237955ed
Jonas Dalesjö <jonas.dalesjo@gmail.com>
2025-11-27 02:21:19 +0100

    Env

--- stack.ts
+++ stack.ts
@@ -2,6 +2,7 @@ import 'server-only';
 import { StackServerApp } from '@stackframe/stack';
 
 export const stackServerApp = new StackServerApp({
+  projectId: process.env.NEXT_PUBLIC_STACK_PROJECT_ID,
   tokenStore: 'nextjs-cookie',
   urls: {
     signIn: '/handler/sign-in',
